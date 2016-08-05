d7d608fbc9189d49fd1c73d55638e7ed318f87e3
Gumichem <gumichem@gmail.com>
2016-08-06 01:52:10 +0300

    Calculate update

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,12 @@
-public class Calculate{
-    public static void main(String[] args){
+/**
+ * Created by Olya on 06.08.2016.
+ */
+public class Calculate {
+    public static void main (String[] args){
         System.out.println("Calculate...");
+        int first = Integer.valueOf(args[0]);
+        int second = Integer.valueOf(args[1]);
+        int summ = first+second;
+        System.out.println(summ);
     }
 }
